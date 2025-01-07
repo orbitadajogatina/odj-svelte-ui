bebff2b705dd7105ff9289955555405e0baa5c83
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-07 20:47:39 -0300

    badge

--- src/routes/oldstuff/+page.svelte
+++ src/routes/oldstuff/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-  import { Input, Alert } from '$lib/elements';
+  import { Input, Alert, Badge } from '$lib/elements';
 </script>
 
 <Input type="text" />
-<Alert color="red">ABCDEF...</Alert>
+<Alert color="red" rounded dismissable>ABCDEF...</Alert>
+<Badge dismissable/>
