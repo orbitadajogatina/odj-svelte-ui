869a13832db291942806af7a7fbed50e7c92f5da
orbitadajogatina <orbitadajogatina@gmail.com>
2025-02-05 00:28:39 -0300

    border slightly thicker on select

--- src/lib/forms/select/theme.ts
+++ src/lib/forms/select/theme.ts
@@ -7,7 +7,7 @@ export const select = tv({
   variants: {
     underline: {
       true: "text-light-surface-500 bg-transparent border-0 border-b-2 border-light-surface-200 appearance-none dark:text-dark-surface-400 dark:border-dark-surface-700 focus:outline-none focus:ring-0 focus:border-light-surface-200 peer !px-0",
-      false: "text-light-surface-900 bg-light-surface-50 border border-light-surface-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-dark-surface-700 dark:border-dark-surface-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:placeholder-dark-surface-400 required:invalid:text-dark-surface-400"
+      false: "text-light-surface-900 bg-light-surface-50 border border-light-surface-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-dark-surface-700 dark:border-dark-surface-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:placeholder-dark-surface-400 required:invalid:text-dark-surface-400 outline-none focus:ring-1"
     },
     size: {
       sm: "text-sm p-2",
