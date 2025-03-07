8fc8390c8730fd3ca917a3bc8261c290c5063f1d
orbitadajogatina <orbitadajogatina@gmail.com>
2025-03-07 15:31:22 -0300

    refactor and style

--- src/lib/tooltip/Tooltip.svelte
+++ src/lib/tooltip/Tooltip.svelte
@@ -1,43 +1,34 @@
 <script lang="ts">
   import { tooltip } from "./theme";
   import type { TooltipProps } from ".";
-  import {
-    FloatingArrow,
-    arrow,
-    autoUpdate,
-    flip,
-    offset as floatingUIOffset,
-    useDismiss,
-    useFloating,
-    useHover,
-    useInteractions,
-    useRole,
-  } from "@skeletonlabs/floating-ui-svelte";
+  import { FloatingArrow, arrow, autoUpdate, flip, offset as floatingUIOffset, useDismiss, useFloating, useHover, useInteractions, useRole } from "@skeletonlabs/floating-ui-svelte";
   import { fade } from "svelte/transition";
 
-  let { children, color = "default", arrow: showArrow = false, offset = 8, triggeredBy, position = "top", class: className, arrowClass, open = $bindable(false), transition = fade, transitionOptions = { duration: 200 }, ...restProps }: TooltipProps = $props();
+  let { children, color = "default", arrow: showArrow = false, offset = 8, triggeredBy, position: placement = "top", class: className, arrowClass, open = $bindable(false), transition = fade, transitionOptions = { duration: 200 }, ...restProps }: TooltipProps = $props();
 
   let arrowElement: HTMLElement | null = $state(null);
 
   let { base, arrowBase } = $derived(tooltip({ color }));
-  
+
   // State
   let elemArrow: HTMLElement | null = $state(null);
 
   // Use Floating
-  let floating = $derived(useFloating({
-    whileElementsMounted: autoUpdate,
-    get open() {
-      return open;
-    },
-    onOpenChange: (v) => {
-      open = v;
-    },
-    placement: position,
-    get middleware() {
-      return [floatingUIOffset(offset), flip(), elemArrow && arrow({ element: elemArrow })];
-    },
-  }));
+  let floating = $derived(
+    useFloating({
+      whileElementsMounted: autoUpdate,
+      get open() {
+        return open;
+      },
+      onOpenChange: (v) => {
+        open = v;
+      },
+      placement,
+      get middleware() {
+        return [floatingUIOffset(offset), flip(), elemArrow && arrow({ element: elemArrow })];
+      }
+    })
+  );
 
   // Interactions
   let role = $derived(useRole(floating.context, { role: "tooltip" }));
@@ -48,22 +39,22 @@
   // Get elements
   $effect(() => {
     let triggerElement = document.querySelector(triggeredBy);
-    if (!triggerElement) return; 
+    if (!triggerElement) return;
 
     floating.elements.reference = triggerElement;
-    
+
     // Assign the props to the element
     Object.entries(interactions.getReferenceProps()).forEach(([key, value]) => {
-      triggerElement[key] = value;
+      (triggerElement as any)[key] = value;
     });
-  })
+  });
 </script>
 
 {#if open}
   <div role="tooltip" bind:this={floating.elements.floating} style={floating.floatingStyles} {...interactions.getFloatingProps()} transition:transition={transitionOptions} {...restProps} class={base({ className })}>
     {@render children()}
     {#if showArrow}
-      <FloatingArrow bind:ref={arrowElement} context={floating.context} class={arrowBase({className: arrowClass})}/>
+      <FloatingArrow bind:ref={arrowElement} context={floating.context} class={arrowBase({ className: arrowClass })} />
     {/if}
   </div>
 {/if}
@@ -75,7 +66,7 @@
 @props:children: any;
 @props:color: any = "default";
 @props:arrow: any = false;
-@props:offset: any = 8;
+@props:offset: number = 8;
 @props:triggeredBy: any;
 @props:position: string = "top";
 @props:class: string;
