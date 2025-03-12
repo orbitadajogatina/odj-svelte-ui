af443415484c0b95253cf073f1a8c15f130b90c9
orbitadajogatina <orbitadajogatina@gmail.com>
2025-03-11 22:32:08 -0300

    docs: ondragover and showing better uploaded files

--- src/routes/forms/file-input/examples/Dropzone.svelte
+++ src/routes/forms/file-input/examples/Dropzone.svelte
@@ -35,14 +35,14 @@
   const showFiles = (files: string[]): string => {
     if (files.length === 1) return files[0];
     let concat = files.join(", ");
-    if (concat.length > 40) {
-      concat = concat.slice(0, 40) + "...";
-    }
+    // if (concat.length > 40) {
+    //   concat = concat.slice(0, 40) + "...";
+    // }
     return concat;
   };
 </script>
 
-<Dropzone multiple id="dropzone" ondrop={dropHandle} onchange={handleChange}>
+<Dropzone multiple id="dropzone" ondrop={dropHandle} onchange={handleChange} ondragover={() => console.log('dropping...')}>
   <svg aria-hidden="true" class="mb-3 h-10 w-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
   {#if value.length === 0}
     <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span></p>
