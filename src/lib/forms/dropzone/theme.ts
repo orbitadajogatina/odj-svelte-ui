e999ff1d13f99b41474bc1f1e0f8d6996702b2d2
orbitadajogatina <orbitadajogatina@gmail.com>
2025-03-11 22:30:55 -0300

    fix: removing hard-coded gray

--- src/lib/forms/dropzone/theme.ts
+++ src/lib/forms/dropzone/theme.ts
@@ -1,5 +1,5 @@
 import { tv } from "tailwind-variants";
 
 export const dropzone = tv({
-  base: "flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+  base: "flex flex-col justify-center items-center w-full h-64 bg-light-surface-50 rounded-lg border-2 border-light-surface-300 border-dashed cursor-pointer dark:hover:bg-dark-surface-800 dark:bg-dark-surface-700 hover:bg-light-surface-100 dark:border-dark-surface-600 dark:hover:border-dark-surface-500 dark:hover:bg-dark-surface-600 focus-visible:ring-4 focus-visible:outline-hidden focus-visible:ring-primary-300 dark:focus-visible:ring-primary-800"
 });
