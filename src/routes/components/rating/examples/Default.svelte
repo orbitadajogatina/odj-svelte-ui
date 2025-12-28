779d47446179f3508ad11c9c53972cac712d156b
orbitadajogatina <orbitadajogatina@gmail.com>
2025-12-28 17:24:52 -0300

    docs: updated example

--- src/routes/components/rating/examples/Default.svelte
+++ src/routes/components/rating/examples/Default.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import { Rating } from "$lib";
+  import Button from "$lib/buttons/Button.svelte";
+
+  let random = $state(5);
 </script>
 
-<Rating total={5} size={30} rating={1.4} />
-<Rating total={5} size={30} rating={4.66} />
+<Rating total={5} size={30} rating={random / 2} />
+<Rating total={5} size={30} rating={random} iconProps={{ onclick: (event) => alert(event.currentTarget.getAttribute("data-icon-index")) }} />
+
+<Button onclick={() => (random = Math.floor(Math.random() * 5))}>Randomize</Button>
